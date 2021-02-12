aa111266e9f601f507c3d68b56e004e75e7f9c8d
chuehlien <lien_julia@bah.com>
2021-02-12 13:34:20 -0500

    Add read_csv method

--- datahub_metrics_ingest/util.py
+++ datahub_metrics_ingest/util.py
@@ -1,7 +1,22 @@
 import csv
+import io
 
 from datahub_metrics_ingest.DHMetric import DHMetric
 
+
+def read_csv(infile: io.TextIOWrapper) -> list[dict[str, str]]:
+    recs = []
+    process_line = lambda line: [i.strip() for i in line.split(',')]
+    
+    line = infile.readline()
+    headers = process_line(line)
+
+    line = infile.readline()
+    while line:
+        row = dict(zip(headers, process_line(line)))
+        recs.append(row)
+        line = infile.readline()
+    return recs
 
 def write_metrics_to_csv(fp: str, metric_objs: [DHMetric]):
     headers = "timestamp,granularity,dh_id,dh_source_name,user_segment,access_type,count".split(',')
