d03c3a1d3f1b4e980c1daec56b223b5ae52dd4f4
chuehlien <lien_julia@bah.com>
2021-02-16 16:52:36 -0500

    Fix typo and add typing

--- datahub_metrics_ingest/ElasticsearchDAO.py
+++ datahub_metrics_ingest/ElasticsearchDAO.py
@@ -2,12 +2,14 @@ import os
 import requests
 import json
 
+from datahub_metrics_ingest.DHMetric import DHMetric
+
 
 class ElasticsearchDAO(object):
-    def __init__(self, elasticsearch_api_base_url=None):
+    def __init__(self, elasticsearch_api_base_url: str = None):
         self.elasticsearch_api_base_url = elasticsearch_api_base_url
 
-    def write_to_elasticsearch(self, metrics):
+    def write_to_elasticsearch(self, metrics: [DHMetric]):
 
         document = ''
         for metric in metrics:
@@ -26,19 +28,19 @@ class ElasticsearchDAO(object):
             es_post_response = requests.post(f'{self.elasticsearch_api_base_url}/_bulk', data=document, headers=header)
             print(f'Data written to ES: {es_post_response.status_code} {es_post_response.reason} ({len(metrics)})')
 
-    def map_obj_doc(self, metric, line_obj_doc):
+    def map_obj_doc(self, metric: DHMetric, line_obj_doc):
         new_lineobj_doc = {}
 
         new_lineobj_doc['timestamp'] = metric.timestamp
         new_lineobj_doc['granularity'] = metric.granularity
         new_lineobj_doc['dhId'] = metric.dh_id
         new_lineobj_doc['dhSourceName'] = metric.dh_source_name
-        new_lineobj_doc['userSegment'] = metric.user_segmnet
+        new_lineobj_doc['userSegment'] = metric.user_segment
         new_lineobj_doc['accessType'] = metric.access_type
         new_lineobj_doc['count'] = metric.count
         
         return new_lineobj_doc
     
-    def format_es_id(self, m):
+    def format_es_id(self, m: DHMetric):
         es_id = f'{m.dh_id}::{m.timestamp}::{m.granularity}::{m.access_type}::{m.user_segment}'
         return es_id
